cbb6abe1288d4197f1ae05af816c377e2412a201
Artem Kireev <est1908@gmail.com>
2020-10-27 05:18:46 +0300

    app-connected component refactored to function with hooks

--- src/components/smart/app/app-connected.tsx
+++ src/components/smart/app/app-connected.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ActionButtonCode } from 'components/dumb/action-buttons/action-button-codes';
 import { App } from 'components/dumb/app/app-component';
 import { IBowlingScoreApp, IFrameScore } from '../../../domain';
@@ -7,7 +7,7 @@ type Props = {
     bowlingScore: IBowlingScoreApp;
 };
 
-type State = {
+type AppState = {
     frameScores: IFrameScore[];
     currentFrameIndex: number;
     totalScore: number;
@@ -16,7 +16,42 @@ type State = {
     isUndoAvailable: boolean;
 };
 
-function mapToState(bowlingScore: IBowlingScoreApp): State {
+export default function AppConnected(props: Props) {
+    const [appState, setAppState] = useState(mapToAppState(props.bowlingScore));
+
+    return <App {...appState} onNewGame={handleNewGame} onUndo={handleUndo} onAction={handleAction} />;
+
+    function handleAction(btnCode: ActionButtonCode) {
+        const pins = buttonCodeToPins(btnCode);
+        if (props.bowlingScore.isInputValid(pins)) {
+            props.bowlingScore.add(pins);
+            updateState();
+        }
+    }
+
+    function handleNewGame() {
+        props.bowlingScore.reset();
+        updateState();
+    }
+
+    function handleUndo() {
+        props.bowlingScore.undo();
+        updateState();
+    }
+
+    function updateState() {
+        setAppState(mapToAppState(props.bowlingScore));
+    }
+
+    function buttonCodeToPins(btnCode: ActionButtonCode): number {
+        if (btnCode === '/' || btnCode === 'x') {
+            return appState.pinsAvailable;
+        }
+        return btnCode as number;
+    }
+}
+
+function mapToAppState(bowlingScore: IBowlingScoreApp): AppState {
     return {
         frameScores: bowlingScore.scoreTable.frameScores,
         totalScore: bowlingScore.scoreTable.totalScore,
@@ -25,46 +60,4 @@ function mapToState(bowlingScore: IBowlingScoreApp): State {
         isGameFinished: bowlingScore.isGameFinished,
         isUndoAvailable: bowlingScore.isUndoAvailable
     };
-}
-
-export default class AppConnected extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = mapToState(this.props.bowlingScore);
-    }
-
-    render(): JSX.Element {
-        return (
-            <App {...this.state} onNewGame={this.handleNewGame} onUndo={this.handleUndo} onAction={this.handleAction} />
-        );
-    }
-
-    handleAction = (btnCode: ActionButtonCode): void => {
-        const pins = this.buttonCodeToPins(btnCode);
-        if (this.props.bowlingScore.isInputValid(pins)) {
-            this.props.bowlingScore.add(pins);
-            this.updateState();
-        }
-    };
-
-    handleNewGame = (): void => {
-        this.props.bowlingScore.reset();
-        this.updateState();
-    };
-
-    handleUndo = (): void => {
-        this.props.bowlingScore.undo();
-        this.updateState();
-    };
-
-    private updateState() {
-        this.setState(mapToState(this.props.bowlingScore));
-    }
-
-    private buttonCodeToPins(btnCode: ActionButtonCode): number {
-        if (btnCode === '/' || btnCode === 'x') {
-            return this.state.pinsAvailable;
-        }
-        return btnCode as number;
-    }
 }
